7018a0eba23dfb54df73f0fd1a654b1543871623
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-01-16 16:18:24 +0000

    Added 01 Basics of DS

---
+++ 01_Basics/Introduction.java
@@ -0,0 +1,3 @@
+// Q1 What is data structure?
+// Ans. Data Structure is a particular way of organizing managing and storing the data,
+//      in the computer system so that it can be effectively accessed and managed.
